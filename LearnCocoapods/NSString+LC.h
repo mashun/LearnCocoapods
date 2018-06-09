a3518cfa632d8f158c7704acac346b82e70aa5a0
马顺 <978292663@qq.com>
2018-06-09 16:24:28 +0800

    add

---
+++ LearnCocoapods/NSString+LC.h
@@ -0,0 +1,13 @@
+//
+//  NSString+LC.h
+//  Demo
+//
+//  Created by 马顺 on 2018/6/9.
+//  Copyright © 2018年 shunzi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (LC)
+
+@end
---
+++ LearnCocoapods/NSString+LC.m
@@ -0,0 +1,13 @@
+//
+//  NSString+LC.m
+//  Demo
+//
+//  Created by 马顺 on 2018/6/9.
+//  Copyright © 2018年 shunzi. All rights reserved.
+//
+
+#import "NSString+LC.h"
+
+@implementation NSString (LC)
+
+@end
